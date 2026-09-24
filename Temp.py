5957de9304d9b8c022fafc4fd12749d8de554fe7
Piyush Mehta <piyushmehta1187@gmail.com>
2026-09-24 20:26:41 +0530

    Update Temp.py

--- Temp.py
+++ Temp.py
@@ -9,19 +9,19 @@ choice = int(input("Enter your choice: "))
 if choice == 1:
     c = float(input("Enter Celcius: "))
     f = (c * 9 / 5) + 32
-    print("Result is :")
+    print("Result :")
     print(c, "C =", f, "F")
 
 elif choice == 2:
     f = float(input("Enter Farenheit: "))
     c = (f - 32) * 5 / 9
-    print("Result is :")
+    print("Result :")
     print(f, "F =", round(c), "C")
 
 elif choice == 3:
     k = float(input("Enter Kelvin: "))
     c = k - 273.15
-    print("Result is :")
+    print("Result :")
     print(k, "K =", round(c, 2), "C")
 
 else:
